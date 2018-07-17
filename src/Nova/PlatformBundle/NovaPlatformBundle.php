03a6c4936f6e6c9e4e690c9a8806c1e601cff27f
blasius <nduwimanabalaise@gmail.com>
2018-07-17 17:01:36 +0300

    Fix problem of routing after Platform bundle generation

---
+++ src/Nova/PlatformBundle/NovaPlatformBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Nova\PlatformBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class NovaPlatformBundle extends Bundle
+{
+}
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -10,13 +10,13 @@ class ComposerStaticInit05bd01e126057962c5a678cb9167eb06
         '92c8763cd6170fce6fcfe7e26b4e8c10' => __DIR__ . '/..' . '/symfony/phpunit-bridge/bootstrap.php',
         '2c102faa651ef8ea5874edb585946bce' => __DIR__ . '/..' . '/swiftmailer/swiftmailer/lib/swift_required.php',
         '5255c38a0faeba867671b61dfda6d864' => __DIR__ . '/..' . '/paragonie/random_compat/lib/random.php',
-        '023d27dca8066ef29e6739335ea73bad' => __DIR__ . '/..' . '/symfony/polyfill-php70/bootstrap.php',
-        'bd9634f2d41831496de0d3dfe4c94881' => __DIR__ . '/..' . '/symfony/polyfill-php56/bootstrap.php',
-        '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
-        '6a47392539ca2329373e0d33e1dba053' => __DIR__ . '/..' . '/symfony/polyfill-intl-icu/bootstrap.php',
-        '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
         '32dcc8afd4335739640db7d200c1971d' => __DIR__ . '/..' . '/symfony/polyfill-apcu/bootstrap.php',
-        '719971e375036615a0685b6fb054583d' => __DIR__ . '/..' . '/symfony/symfony/src/Symfony/Component/VarDumper/Resources/functions/dump.php',
+        '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
+        '6a47392539ca2329373e0d33e1dba053' => __DIR__ . '/..' . '/symfony/polyfill-intl-icu/bootstrap.php',
+        '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
+        'bd9634f2d41831496de0d3dfe4c94881' => __DIR__ . '/..' . '/symfony/polyfill-php56/bootstrap.php',
+        '023d27dca8066ef29e6739335ea73bad' => __DIR__ . '/..' . '/symfony/polyfill-php70/bootstrap.php',
+        '9dbc32f2cc1fcb745bbbe7f32bc9afa9' => __DIR__ . '/..' . '/symfony/symfony/src/Symfony/Component/VarDumper/Resources/functions/dump.php',
     );
 
     public static $prefixLengthsPsr4 = array (
@@ -78,10 +78,6 @@ class ComposerStaticInit05bd01e126057962c5a678cb9167eb06
         'C' => 
         array (
             'Composer\\CaBundle\\' => 18,
-        ),
-        'A' => 
-        array (
-            'AppBundle\\' => 10,
         ),
     );
 
@@ -226,10 +222,10 @@ class ComposerStaticInit05bd01e126057962c5a678cb9167eb06
         array (
             0 => __DIR__ . '/..' . '/composer/ca-bundle/src',
         ),
-        'AppBundle\\' => 
-        array (
-            0 => __DIR__ . '/../..' . '/src/AppBundle',
-        ),
+    );
+
+    public static $fallbackDirsPsr4 = array (
+        0 => __DIR__ . '/../..' . '/src',
     );
 
     public static $prefixesPsr0 = array (
@@ -294,6 +290,7 @@ class ComposerStaticInit05bd01e126057962c5a678cb9167eb06
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit05bd01e126057962c5a678cb9167eb06::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit05bd01e126057962c5a678cb9167eb06::$prefixDirsPsr4;
+            $loader->fallbackDirsPsr4 = ComposerStaticInit05bd01e126057962c5a678cb9167eb06::$fallbackDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit05bd01e126057962c5a678cb9167eb06::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit05bd01e126057962c5a678cb9167eb06::$classMap;
 
